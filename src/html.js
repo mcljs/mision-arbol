fdd61a2311249f234978846838c13122b2e7e132
Michael Chacon <mcljs15@gmail.com>
2021-03-22 03:12:37 -0400

    fixed

--- src/html.js
+++ src/html.js
@@ -27,7 +27,7 @@ export default function HTML(props) {
                 position: "absolute",
                 left: 0,
                 top: 0,
-                marginBottom: '20px',
+                paddingBottom: '3rem',
                 right: 0,
                 bottom: 0,
                 zIndex: 100,
