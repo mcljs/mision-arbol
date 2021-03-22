9ffda8a9c5b73092ff0cac84b4ec3a38b39b06d4
Michael Chacon <mcljs15@gmail.com>
2021-03-22 01:16:07 -0400

    suspense test

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,14 +6,17 @@
  */
 
 
-import React from 'react';
-import Navbar from '../components/Navbar'
-import Footer from './Footer/Footer';
+import React, {useEffect, useState} from 'react';
+
 import {fonts} from '../lib/typography'
 import {Global,css} from '@emotion/react'
 import {bpMaxSM} from '../lib/breakpoints'
 import reset from '../lib/reset'
+import RouteLoadingIndicator from './RouterLoadingIndicator';
 //import "./layout.css"
+const Navbar = React.lazy(()=> import('../components/Navbar'))
+const Footer = React.lazy(()=> import('./Footer/Footer'))
+
 export const globalStyles = css`
    ${bpMaxSM} {
     h1 {
@@ -120,18 +123,48 @@ const Layout = ({ children }) => {
 
 
 
+const [state, setState] = useState({
+    isRouteChanging: false,
+    loadingKey: 0,
+  })
+
+useEffect(() => {
+    const handleRouteChangeStart = () => {
+      setState((prevState) => ({
+        ...prevState,
+        isRouteChanging: true,
+        loadingKey: prevState.loadingKey ^ 1,
+      }))
+    }
+
+const handleRouteChangeEnd = () => {
+      setState((prevState) => ({
+        ...prevState,
+        isRouteChanging: false,
+      }))
+    }
 
 
+  }, [])
+
+const isSSR = typeof window === "undefined"
 
   return (
     <>
-  <Global styles={globalStyles}/>
+     
+      {!isSSR &&(
+ <React.Suspense fallback={ <RouteLoadingIndicator isRouteChanging={state.isRouteChanging}/>}>
+     <Global styles={globalStyles}/>
      <Navbar  />
   
    
 
         <main>{children}</main>
         <Footer />
+
+      </React.Suspense>
+      )}
+
    
     </>
   )
