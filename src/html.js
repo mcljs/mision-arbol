8e2e7ed2a3f1d02137dd1225e370b2f6bd65d1c9
Michael <mcljs15@gmail.com>
2021-04-01 00:51:25 -0400

    fixed

--- src/html.js
+++ src/html.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import LoaderSVG from './images/loader.svg'
+
 
 export default function HTML(props) {
   return (
@@ -20,19 +20,12 @@ export default function HTML(props) {
               key={`loader`}
               id="___loader"
               style={{
-                alignItems: "center",
-                backgroundColor: "#f9fafc",
+
                 display: "flex",
-                justifyContent: "center",
-                position: "absolute",
-                left: 0,
-                top: 0,
-                right: 0,
-                bottom: 0,
-                zIndex: 100,
+                
               }}
            >
-              <img src={LoaderSVG} alt="loader spinner" width="150" height="150"/>
+            
         </div>
         <div
           key={`body`}
