4bc936ba0a4005754eb801a4b48cdaea95bee871
Michael <mcljs15@gmail.com>
2021-04-01 00:08:57 -0400

    fixed

--- src/components/Loaded.js
+++ src/components/Loaded.js
@@ -2,20 +2,7 @@ import React from 'react'
 import LoaderSVG from '../images/loader.svg'
 const Loaded = (  ) => (
 
-    <div
-              style={{
-                alignItems: "center",
-                backgroundColor: "#f9fafc",
-                display: "flex",
-                justifyContent: "center",
-                position: "absolute",
-                left: 0,
-                top: 0,
-                right: 0,
-                bottom: 0,
-                zIndex: 100,
-              }}
-           >
+    <div className="flex items-center bg-[#f9fafc] justify-center absolute z-100 inset-0">
               <img src={LoaderSVG} alt="loader spinner" width="150" height="150"/>
         </div> 
 )
