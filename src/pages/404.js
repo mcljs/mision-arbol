9f69d579cd5dd82ae2fc46f72d5aa482cfdb1788
Michael <mcljs15@gmail.com>
2021-04-03 22:52:00 -0400

    Data Especies

--- src/pages/404.js
+++ src/pages/404.js
@@ -15,11 +15,20 @@ const NotFoundPage = () => (
 		<div className="p-4 md:p-12 text-center lg:text-left">
 					<img src="https://source.unsplash.com/MP0IUfwrn0A" className="block lg:hidden  shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" alt="img" />
 			 <span class="inline-block py-1 px-2 rounded-full bg-green-600 text-white  text-xs font-bold tracking-widest mb-2"> Forestales</span>
-			<h1 className="text-3xl font-bold pt-8 lg:pt-0">Saman</h1>
+			<h1 className="text-3xl font-bold pt-8 lg:pt-0">Vera</h1>
 			<div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
 		
-			<p className="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start"><svg class="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z"/></svg> Localización - 25.0000° N, 71.0000° W</p>
-			<p className="pt-8 text-sm">El samán es un arbol comunmente llamado el arbol de lluvia. Otros de sus nombres mas comunes son mimosa saman, samanea saman, Carabalí, Urero, Cenicero y cientificamente Samán Pithecellobium. Pertenece a la familia de los Mimosaceae. Abunda en las tierras calientes, se reproduce por semillas y es de rapido crecimiento.</p>
+      <p className="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start"><svg class="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z"/></svg><span>TITULO CIENTIFICO:</span> BULNESIA ARBÓREA</p>
+      <p className="pt-8 text-sm"><span>CARACTERISTICAS FÍSICAS: </span>ES UN ÁRBOL DE GRAN PARTE DEL NORTE DE SUDAMÉRICA, EN LA REGIÓN DE LAS GUAYANAS, DE VENEZUELA, COLOMBIA, GUYANA. A VECES EN EL MERCADO INTERNACIONAL SE LO NOMBRA COMO PARAGUAY LIGNUM VITAE YA QUE SUS PROPIEDADES Y USOS SON SIMILARES AL GUAYACÁN, ÁRBOL DEL GÉNERO GUAIACUM.
+
+CRECE EN BORDES DE CARRETERAS, CAMINOS Y RÍOS, Y EN EL INTERIOR DE BOSQUES SECOS, SUBXEROFÍTICOS Y ESPINOSOS.
+</p>
+ <p className="pt-8 text-sm"><span>PROPIEDADES: </span>MUY USADO EN CONSTRUCCIONES CIVILES Y EN NAVALES, TORNERÍA, ELABORACIÓN DE DURMIENTES DE FERROCARRIL. ES PLANTA ORNAMENTAL EN JARDINERÍA. LA MADERA DEBE UTILIZARSE RECIÉN CORTADA, PUESTO QUE AL SECARSE SE PONE EXTREMADAMENTE DURA Y YA NO ES APROVECHABLE.
+LA VERA SE EMPLEA PARA GRABADOS Y CUANDO SE NECESITA UNA MADERA DURABLE. DE SU MADERA SE EXTRAE EL ACEITE DE GUAIAC (O GUAYACOL) PARA INGREDIENTE DE PERFUMES. SU RESINA PUEDE OBTENERSE CON SOLVENTES ORGÁNICOS, EMPLEADA PARA HACER BARNIZ Y PINTURAS OSCURAS.
+ES APRECIADO POR SUS PROPIEDADES PROTECTORAS DE LA PIEL HUMANA CON SU ESENCIA. DA UN BUEN CARBÓN Y VIGAS DE ALTA CALIDAD. IGNICIONA FÁCILMENTE, Y PRODUCE UN HUMO FRAGANTE.
+LAS DOS ESPECIES MÁS CONOCIDAS SON LA BULNESIA ARBÓREA Y LA BULNESIA SARMIENTOI.
+
+</p>
 <div className="pt-12 pb-8">
 				<button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
 				  Regresar
