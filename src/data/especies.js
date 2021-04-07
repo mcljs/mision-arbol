5dca0d343797ce386001b1a6e5c29d3f80108e19
Michael Chacon <mcljs15@gmail.com>
2021-04-07 16:17:46 -0400

    new posts guide

--- src/data/especies.js
+++ src/data/especies.js
@@ -155,7 +155,7 @@ Los remedios con pino albar se destinan básicamente a aliviar afecciones respir
     title: 'Cedro Americano',
     publisher: 'Forestales',
     title_cientifico: 'CEDRELA ODORATA',
-    image: 'cedro-americano.jpeg',
+    image: 'cedro-americano.jpg',
     imageURL: 'https://i.pinimg.com/originals/15/ab/02/15ab02028e2fdf78b78b4b50132f11ec.jpg',
     caracteristicas_fisicas: `
 El cedro amargo tiene un fuste que puede alcanzar los 40 m de altura; el tronco es recto, naciendo sus ramas, van más arriba de la mitad de su altura y con diámetros en los árboles adultos de 1 a 2 m; a veces, en su parte baja presenta contrafuertes o aletones que ayudan a afianzar el árbol, ya que tiene un sistema radical bastante superficial.
@@ -195,6 +195,162 @@ Ormosia coccinea se desarrolla como un árbol grande, de hasta 30 m de altura, d
 Las flores son purpúreas, aladas y cerradas como se presentan en las legumbres; el fruto es una pequeña legumbre o vaina aplanada, punteada en un extremo y de color verde y rojo-anaranjado; en el interior se encuentran dentro de una pulpa dulce de una a dos semillas que son negras en una mitad y rojas en la otra.`,
     propiedades:`
 Esas semillas se usan en joyería y otras decoraciones; son venenosas si se ingieren.  `,
+
+  },
+  {
+    id: 'pionia',
+    title: 'Pionia',
+    publisher: 'Forestales',
+    title_cientifico: 'ORMOSIA COCCINEA',
+    image: 'pionia.jpg',
+    imageURL: 'https://i.pinimg.com/originals/69/c3/01/69c301b1c2b117ebd562052ad5e55e58.jpg',
+    caracteristicas_fisicas: `
+Ormosia coccinea se desarrolla como un árbol grande, de hasta 30 m de altura, de zonas boscosas con un tronco cilíndrico vertical, de hasta 9 dm de diámetro; a lo largo del tronco se presentan anillos horizontales; la corteza es marrón o negruzca.
+
+Las flores son purpúreas, aladas y cerradas como se presentan en las legumbres; el fruto es una pequeña legumbre o vaina aplanada, punteada en un extremo y de color verde y rojo-anaranjado; en el interior se encuentran dentro de una pulpa dulce de una a dos semillas que son negras en una mitad y rojas en la otra.`,
+    propiedades:`
+Esas semillas se usan en joyería y otras decoraciones; son venenosas si se ingieren.  `,
+
+  },
+  {
+    id: 'merey',
+    title: 'Merey',
+    publisher: 'Forestales',
+    title_cientifico: 'ANACARDIUM OCCIDENTALE',
+    image: 'merey.jpg',
+    imageURL: 'https://u.iplantz.com/80/Anacardium%20occidentale-9957.5501.jpg',
+    caracteristicas_fisicas: `
+El merey en realidad es el fruto del árbol del merey; el fruto fresco es verde; la nuez tiene forma parecida al frijol y alcanza, aproximadamente, 2,5 centímetros (1 pulgada) de largo; el merey tiene dos capas de cáscara, entre estas capas hay un aceite fuerte que puede producir ampollas en la piel.`,
+    propiedades:`
+La nuez del merey se puede hervir, asar o tostar y se come como bocadillo en muchas partes del mundo; también se muele para hacer mantequilla de nuez o se utilizan en distintos caramelos y otros dulces; en india y en asia sudoccidental, a menudo se incorpora merey en diversos platos de comida; la nuez es rica en proteínas y grasas. `,
+
+  },
+  {
+    id: 'fruto-de-burro',
+    title: 'Fruto de Burro',
+    publisher: 'Forestales',
+    title_cientifico: 'XYLOPIA AROMATICA',
+    image: 'fruto-de-burro.jpg',
+    imageURL: 'http://repository.humboldt.org.co/bitstream/handle/20.500.11761/883/26985.jpg?sequence=1&isAllowed=y',
+    caracteristicas_fisicas: `
+Es un árbol nativo de la vegetación de pastizales cerrado, especialmente en los estados de goiás y minas gerais, en el este de brasil; también se halla en venezuela, hacia el sur del estado monagas.
+Mide entre 15 a 25 m de altura, su corteza es lisa y es fácil de desprender en forma de guasca, sus hojas elípticas son simples y alternas, posee flores agrupadas en inflorescencias axilares, sus flores poseen seis pétalos y fuerte olor, los frutos están agrupados en fascículos de unas 20 unidades.`,
+    propiedades:`
+En Colombia la infusión de la planta entera es usada por los indios witoto como un diurético fuerte (schultes, 1980), sus frutos se utilizan como tónico uterino para corregir la dismenorrea (garcía barriga, 1974); en brasil los frutos secos se utilizan como sustituto de la pimienta y; la decocción de las hojas secas se usa como un diurético fuerte, para reducir el edema de los miembros inferiores; (fournier, et al, 1994). `,
+
+  },
+  {
+    id: 'corozo',
+    title: 'Corozo',
+    publisher: 'Forestales',
+    title_cientifico: 'BACTRIS GUINEENSIS',
+    image: 'fruto-de-burro.jpg',
+    imageURL: 'https://static.inaturalist.org/photos/4193126/large.JPG?1467767230',
+    caracteristicas_fisicas: `
+Es una palma cespitosa con hasta cien estípites erectos de 3 a 5 m de altura y 1,5 a 3 cm de diámetro, sus hojas están esparcidas a lo largo del tallo, la vaina de 30 a 60 cm de longitud tiene aguijones delgados de 1 cm de largo, el pecíolo de 4 a 7 cm tiene espinas de 3 a 6 cm, también presentes en el raquis, el cual mide 25 a 80 cm de longitud y tiene de 19 a 42 pinnas de 20 a 30 cm a cada lado, flores de 4 a 6 mm estaminadas y pistiladas, el soporte de la inflorescencia tiene hasta 20 cm de largo, con una bráctea amarillenta encima. Fruto en racimo, globoso deprimido, color negro violáceo, de 1,5 a 2 cm de diámetro.`,
+    propiedades:`
+La madera de estas palmas se utiliza en la fabricación de diferentes objetos. De algunas de las especies de estos árboles del corozo, su madera es utilizada para la construcción de viviendas, y en otros aspectos relacionados con el hábitat; también a base de su madera se elaboran las conocidas flautas, que denomina flauta de millo, que se emplea como instrumento en la cumbia, las gaitas y otros géneros musicales.
+
+Los frutos aparecen en las copas de estas palmas que pueden tener hasta 3 o 5 metros de ancho;  se trata de una especie de fruta ciruela, arándano o incluso puede llegar a tener una apariencia similar al acai de fuerte tradición gastronómica desde la llegada de los esclavos africanos. `,
+
+  },
+  {
+    id: 'palma-real',
+    title: 'Palma Real',
+    publisher: 'Forestales',
+    title_cientifico: 'ROYSTONEA REGIA',
+    image: 'palma-real.jpg',
+    imageURL: 'https://st3.depositphotos.com/31856992/34580/i/600/depositphotos_345802902-stock-photo-tall-royal-palm-trees-roystonea.jpg',
+    caracteristicas_fisicas: `
+La palma real tiene muchos usos, además de su popular cultivo en jardinería, el tronco proporciona tablas para los "bohíos" o casas campesinas, las grandes hojas o pencas conocidas como guano sirven para techar casas; con las espatas florales se hacen "catauros", que son como cestas no tejidas. Las vainas foliares o "yaguas" sirven para envolver las hojas curadas del tabaco en tercios y antes también eran muy usadas para hacer las paredes de los bohíos. Sus flores son una importante fuente de alimento para las abejas; su fruto, el palmiche, cuelga muy abundantemente en racimos y brindan excelente alimento para cerdos. Los racimos ya secos son útiles como escobas rústicas.`,
+    propiedades:`
+En Cuba se usa la raíz en cocimiento como diurético, para expulsar piedras en la orina, para el tratamiento de la diabetes, y se dice que tiene propiedades emolientes.`,
+
+  },
+  {
+    id: 'moriche',
+    title: 'Moriche',
+    publisher: 'Forestales',
+    title_cientifico: 'MAURITIA FLEXUOSA',
+    image: 'moriche.jpg',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/2/23/Mauritia_flexuosa_%2819861732855%29.jpg',
+    caracteristicas_fisicas: `
+Mauritia flexuosa es una especie de palmera perteneciente a la familia arecaceae, recibe los nombres comunes de palma de moriche o simplemente Moriche (en Bolivia, Colombia, Puerto Rico y Venezuela), Aguaje (en Perú), Burití (en Brasil), o Morete (en Ecuador).`,
+    propiedades:`
+El tallo tierno se puede cortar para extraer palmitos comestibles, y de las hojas se extraen fibras para fabricar cordeles, cestas, chinchorros y otros objetos.`,
+
+  },
+  {
+    id: 'araguaney',
+    title: 'Araguaney',
+    publisher: 'Forestales',
+    title_cientifico: 'TABEBUIA CHRYSANTHA',
+    image: 'araguaney.jpg',
+    imageURL: 'https://i.pinimg.com/originals/25/d6/9a/25d69a39aed9787fad88f62763222744.jpg',
+    caracteristicas_fisicas: `
+Es un árbol que alcanza hasta 5 m de alto o más; el tronco puede llegar a tener un diámetro de hasta 60 cm, es caducifolio (que pierde las hojas en condiciones de sequía), ramas escasas gruesas y ascendentes; fuste recto, la corteza es áspera de color gris a café oscuro, tiene grietas verticales, profundas y forman placas anchas de color café oscuro. Hojas opuestas, con 5 hojuelas, de 5 a 25 cm de largo y de 8 a 20 cm de ancho. Sus flores campanuladas (forma de campana), grandes, en grupos de inflorescencias, de 5 a 12 cm de largo, de color amarillo claro, muy vistosas con líneas rojas en el cuello; los frutos son cápsulas cilíndricas, angostas, de 11 a 35 cm de largo y 0,6 a 2 cm de ancho, dehiscentes (que se abre espontáneamente a la madurez). Semillas aladas, aplanadas, de 1,5 a 2 cm de largo y 1 cm de ancho, de color gris plateado.`,
+    propiedades:`
+La corteza tiene principios activos que mediante el cocimiento permite emplearlo para el tratamiento de los síntomas del paludismo. `,
+
+  },
+  {
+    id: 'baraman',
+    title: 'Baramán',
+    publisher: 'Forestales',
+    title_cientifico: 'CATOSTEMMA COMMUNE SANDWITH',
+    image: 'baraman.jpg',
+    imageURL: 'https://c8.alamy.com/compes/bf4gae/comun-baramalli-catostemma-comuna-tronco-entre-vegetacion-selvatica-reserva-de-selva-tropical-de-iwokrama-escudo-guayanes-guyana-bf4gae.jpg',
+    caracteristicas_fisicas: `
+Árbol que puede llegar a alcanzar los 35 metros de altura, de tallo engrosado y brillante, posee un grupo de ramas gruesas casi horizontales en la parte superior, sus flores son amarillentas o rojizas y de forma acampanada, sus frutos están rodeados por cinco alas membranosas de color rosa.`,
+    propiedades:`
+Según la escala de janka es una de las maderas más blandas que existen con un valor de 22 unidades frente a las 5060 del roble-toro (australian buloke = allocasuarina luchmannii); o sea, es 23 veces más blanda. Con su madera se elaboran canoas, artesas, usadas estas últimas para guardar granos, harinas y para lavar; la semilla central del fruto se come tostada; en árboles jóvenes la fibra de su corteza se utiliza como jagua o amarre; en el darién panameño, la madera descompuesta del árbol se utiliza como abono de jardín; de otro lado, ramas y corteza de árboles muertos, se utiliza para alimentar cerdos. `,
+
+  },
+  {
+    id: 'ceiba',
+    title: 'Ceiba',
+    publisher: 'Forestales',
+    title_cientifico: 'CEIBA PENTANDRA',
+    image: 'ceiba.jpg',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/2/21/Ceiba_pentandra_madeira_hg.jpg',
+    caracteristicas_fisicas: `
+Árbol de gran envergadura que puede superar los 60 a 70 metros de altura, con tronco recto, grueso (3 m de diámetro) y normalmente sin ramificar formando una gran copa con ramas extendidas horizontalmente; las raíces tabulares de varios metros de altura y extendidas por encima y por debajo del suelo funcionan como contrafuertes. El tronco y muchas de sus ramas jóvenes están poblados de gruesas espinas cónicas; las hojas palmadas se dividen en 5 a 9 folíolos más pequeños, cada hoja sobrepasa los 20 cm. Flores axilares, solitarias o en fascículos (hasta 15) con 5 pétalos de color blanco, rosa o amarillo. Los frutos son cápsulas dehiscentes de unos 15 cm (a veces mal llamadas vainas, que es otro fruto diferente) que contienen numerosas semillas de unos 6 mm, marrones, de forma más o menos esférica rodeadas por una fibra amarillenta de aspecto algodonoso, mezcla de lignina y de celulosa`,
+    propiedades:`
+En análisis fitoquímicos se revela la presencia de flavonoides y leucoantocianidinas, taninos y lactonas terpénicas. Compuestos estos que le atribuyen usos medicinales importante como antiinflamatorio para heridas, tratamiento de granos, reumatismo, antiespasmódico, vomitivo astringente, antitérmico, antiespasmódico y diurético. ​puede ayudar a controlar hemorragias, diarrea, disentería, congestión bronquial y alivia migrañas, torceduras y heridas abiertas. Las semillas son tóxicas, se usan la corteza, las hojas y la resina; con la corteza se hacen decocciones, las hojas frescas se usan en cataplasmas e infusiones; el exudado de la corteza viva es tenido como abortivo. `,
+
+  },
+  {
+    id: 'bucare',
+    title: 'Bucare',
+    publisher: 'Forestales',
+    title_cientifico: 'ERYTHRINA POEPPIGIANA',
+    image: 'bucare.jpg',
+    imageURL: 'https://static.wixstatic.com/media/54c99b_7714aa8271664b71bfa88094acee3059.jpg',
+    caracteristicas_fisicas: `
+Árbol de 10 hasta 30 m de alto o más. Tronco erecto, cilíndrico; se ramifica más arriba de la mitad de su altura; puede llegar a medir hasta un metro de diámetro en la base, la corteza es de color pardo verdoso o marrón, algo lisa y ligeramente acanalada, con verrugas y a veces con espinas gruesas, la corteza interior es gruesa, relativamente espesa ya que llega a medir hasta 2 cm de espesor, de color blancuzco y de sabor amargo, las ramitas tiernas son verdosas y están cubiertas de pelos. Con el tiempo toman un color gris –verdoso y presentan cicatrices de las hojas desprendidas; a menudo poseen espinas esparcidas, de alrededor 1,5 mm de largo, las hojas son compuestas, alternas, de 20 cm a 30 cm de largo, incluyendo los pecíolos que son finamente vellosos, de color verde-claro y de 6 a 20 centímetros de largo. La hoja está compuesta de tres foliolos, anchamente aovados, obtusos o ligeramente agudos en el ápice y anchamente redondeados en la base; miden de 6 a 18 cm de largo por 5 cm a 15 cm de ancho, a veces mayores; peciolos de 6 mm a 15 mm de largo, con dos glándulas verdes en la base del foliolo terminal y de los dos foliolos laterales. Las hojas suelen aparecer en las ramas tiernas y se desprenden en el periodo de sequía tropical, de enero a mayo; brotan nuevas después de la floración.
+`,
+    propiedades:`
+Se utiliza para reforestaciones, para parques o grandes extensiones verdes, se destaca espléndidamente por el color de sus flores; también se usa en elaboración de cajonería, tableros aglomerados, para alimentación animal, como sombrío de cafetales y cacaotales y para protección de aguas; la madera es bastante liviana, aunque es poco durable por ser susceptible a la pudrición cuando está en contacto con el suelo. `,
+
+  },
+  {
+    id: 'jacaranda',
+    title: 'Jacaranda',
+    publisher: 'Forestales',
+    title_cientifico: 'JACARANDA MIMOSIFOLIA',
+    image: 'jacaranda.jpg',
+    imageURL: 'https://thumbs.dreamstime.com/z/jacaranda-mimosifolia-floreciendo-en-m%C3%A9xico-clima-c%C3%A1lido-el-%C3%A1rbol-florece-la-ciudad-de-abril-170104563.jpg',
+    caracteristicas_fisicas: `
+El árbol adulto alcanza una altura de 12 a 15 metros,hasta 20 metros en condiciones favorables.
+Las raíces, de desarrollo oblicuo, iguales y fasciculadas no son invasoras, por lo que cuando se presenta un periodo de escasez de agua el árbol se ve muy mal.
+La copa del jacarandá no tiene una forma uniforme: algunas veces en forma de una sombrilla, algunas veces de forma piramidal, pero nunca densa.
+El tronco principal tiene una forma algo torcida y tiene una altura de 6 a 9 m y un diámetro de 4 a 7.
+Las hojas son grandes, de 3 a 5 dm de longitud; son completas, opuestas, bipinnadas, con hojuelas de 25 a 30 con pares de folíolos pequeños de forma oval-oblonga, apiculados, de color verde claro y textura de su superficie lisa pubescente. 
+Las flores, de 4 a 5 cm, están agrupadas en panículas terminales erectas, de 20 a 30 cm y son de color azul violeta.
+`,
+    propiedades:`
+La infusión y tintura de flores, hojas y corteza se usa por vía oral para el tratamiento de la disentería amebiana y otras afecciones gastrointestinales agudas; se le atribuye también propiedades antisépticas, antitumoral y espasmolítica.
+La madera es excelente para trabajos de carpintería en interiores. De color claro, vetas cortas y bien marcadas, dibujo parecido al fresno europeo, es liviana (aprox. 450 kg/m³) y muy trabajable. `,
 
   },
 ]
