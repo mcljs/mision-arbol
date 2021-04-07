770f75167bc6db30a42eab9a8745f23df28da73f
Michael Chacon <mcljs15@gmail.com>
2021-04-07 13:59:10 -0400

    new post guide

--- src/data/especies.js
+++ src/data/especies.js
@@ -56,4 +56,40 @@ LAS DOS ESPECIES MÁS CONOCIDAS SON LA BULNESIA ARBÓREA Y LA BULNESIA SARMIENTO
     `,
 
   },
+  {
+    id: 'curari',
+    title: 'Curari',
+    publisher: 'Forestales',
+    title_cientifico: 'HANDROANTHUS SERRATIFOLIUS',
+    image: 'curari.jpg',
+    imageURL: 'https://live.staticflickr.com/8171/8018342446_fbf9f0df13_b.jpg',
+    caracteristicas_fisicas: `Es un árbol de 8-10 m de altura. las hojas de 10-15 cm de largo, palmado-compuestas con foliolos elípticos. las flores, grandes y amarillas de 8 cm de diámetro. el fruto que alcanza los 25 cm de largo tiene semillas aladas de color blanco. la característica que define la especie es la de sus hojas aserradas (de ahí el nombre de serratifolia).   `,
+    propiedades:`
+Se trata de una madera apreciada en el mercado que destaca por su extrema dureza y resistencia al fuego y a las plagas. a veces se comercializa como un "palo de hierro ", o simplemente como" ipê "(todo el género tabebuia ), o como el lapacho (correctamente tabebuia serratifolia ).
+
+Medicinal: la corteza de tabebuia serratifolia contiene más de 20 compuestos activos incluyendo lapachol, quercetina y otros flavonoides. su corteza interna se utiliza como un té de hierbas, para el tratamiento de infecciones fúngicas.
+
+se ha recomendado contra el cáncer.    `,
+
+  },
+  {
+    id: 'jabillo',
+    title: 'Jabillo',
+    publisher: 'Forestales',
+    title_cientifico: 'HURA CREPITANS',
+    image: 'jabillo.jpg',
+    imageURL: 'https://live.staticflickr.com/8171/8018342446_fbf9f0df13_b.jpg',
+    caracteristicas_fisicas: `Es un árbol venenoso para los seres humanos, de la familia de las euforbiáceas, por su gran tamaño, constituye una especie maderable de gran valor económico, es nativa de las regiones intertropicales americanas, tanto en américa del norte y central como en américa del sur, especialmente en las cuencas del orinoco y del amazonas. en méxico se la conoce con los nombres de haba de indio y haba de san ignacio.
+
+Árbol perennifolio que puede alcanzar los 60 m de altura,  su madera es pesada y compacta y sus raíces son bastante superficiales y con contrafuertes, como las de muchas especies forestales de la zona intertropical. el tronco está cubierto de espinas, probablemente desarrolladas como mecanismo de defensa.
+
+Las hojas ovadas y coriáceas alcanzan los 60 cm de ancho, es una especie monoica, con flores rojas sin pétalos, las masculinas surgen en largos racimos, mientras las femeninas surgen solitarias, el fruto es una cápsula similar a una pequeña calabaza de unos 5 a 8 cm de diámetro con dehiscencia explosiva, los 16 carpelos radiales contienen semillas aplanadas de unos 2 cm de diámetro, encerradas entre dos pequeñas valvas curvas de madera muy dura. estas semillas maduran al llegar la época de lluvias y al mojarse estallan, produciendo un ruido muy fuerte, este estallido las puede catapultar a una velocidad de 70 m por segundo; 4 según una fuente pueden alcanzar una distancia de 100 m,5 según otra, unos 45 m del árbol, con una moda de 30 m^2.  `,
+    propiedades:`
+Los frutos pueden consumirse tostados en pequeñas cantidades, pero crudos son 'muy peligrosos por el látex de propiedades purgantes como la pichoga.
+
+Su látex tóxico afecta a los peces adormeciéndolos, por lo que se utiliza para atraparlos. sin embargo, hay aves (loros, por ejemplo) y monos que se alimentan de sus frutos y semillas, especialmente tiernos.
+
+Los caribes usaban las flechas envenenadas con el látex, 6 la madera, llamada "hura" se utiliza para trabajos menores de construcción.    `,
+
+  },
 ]
