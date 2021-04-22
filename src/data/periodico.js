502622c791b97523bc603c62415d0a4875495e3f
Michael <mcljs15@gmail.com>
2021-04-22 16:44:00 -0400

    fix

--- src/data/periodico.js
+++ src/data/periodico.js
@@ -2,7 +2,7 @@ import noticia01 from '../../static/assets/notiarbol/notiarbol001.pdf'
 
 export const periodicoData = [
 {
-    title: 'Edición 001 del periódico NotiÁrbol resalta el plan de desinfección que se llevo a cabo en el Parque los Caobos en Caracas',
+    title: 'Edición 002 del periódico NotiÁrbol resalta la partida fisica de nuestro Ministro Oswaldo Barbera hacia los bosques de la eternidad',
     title2: 'El periódico NotiÁrbol, en su edición Nº 002, segunda del año 2021, resalta la partida fisica de nuestro Ministro Oswaldo Barbera hacia los bosques de la eternidad.',
     fecha: '29/03/2021 01:00 p.m',
     ruta: `https://cdn-124.anonfiles.com/rd3b59sbu4/e6d93878-1619121652/notiarbol-abril.pdf`, 
