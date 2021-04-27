7027673340c8732a52f258e4012aed4aafe04abf
Michael <mcljs15@gmail.com>
2021-04-27 03:33:08 -0400

    fix

--- src/components/Tweet/index.js
+++ src/components/Tweet/index.js
@@ -16,15 +16,15 @@ const isSSR = typeof window === "undefined"
  <React.Suspense fallback={ <DelayedFallback /> }>
           <div className="xl:w-full grid grid-cols-1 xl:grid-cols-10  gap-4 p-2 md:p-8 bg-[#1d9bf0]">
     <div className="flex justify-between mb-4 xl:col-start-3 xl:col-end-8">
-      <h1 className="text-white text-2xl xl:text-3xl"><strong>Twitter</strong></h1>
+      <h1 className="text-white text-center text-2xl xl:text-3xl"><strong>Twitter</strong></h1>
     </div>
    
     <div className=" grid grid-cols-1  xl:col-start-1 xl:col-end-6 ">
-      <span className="text-white text-2xl xl:text-2xl" >Cuenta Oficial Del Presidente de la República Nicolás Maduro Moros</span>
+      <span className="text-white text-center text-2xl xl:text-2xl" >Cuenta Oficial Del Presidente de la República Nicolás Maduro Moros</span>
       <TwitterTimeNico />
     </div>
     <div className="w-full mt-10 xl:mt-9 xl:col-start-6 xl:col-end-11 xl:ml-6">
-      <span className="text-white text-2xl xl:text-2xl ">Cuenta Oficial Fundación Misión Árbol</span>
+      <span className="text-white text-center text-2xl xl:text-2xl ">Cuenta Oficial Fundación Misión Árbol</span>
     <TwitterTimelineEmbed source Type="misionarbolven"
   screenName="misionarbolven"
   options={{height: 400}}/>
