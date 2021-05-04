b16e8011a4a053df59369cc9a6d4b07453402fca
Michael Chacon <mcljs15@gmail.com>
2021-05-03 21:37:43 -0400

    fix

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -17,19 +17,19 @@ const Banner = (  ) => (
         </div>
                <div className="w-full py-10 px-6 text-gray-800 flex flex-col justify-between">
 
-          <h3 className="font-semibold text-lg leading-tight "> Edición 003 del periódico NotiÁrbol resalta a Josué Alejandro Lorca Vega designado como Nuevo Ministro del Poder Popular para el Ecosocialismo</h3>
+          <h3 className="font-semibold text-lg leading-tight "> Edición 004 del periódico NOTIÁRBOL resalta la producción de ocumo, plátanos y lechosa por la unidad "Mi Querencia" en Birongo.</h3>
           <p className="mt-2">
            
-El periódico NotiÁrbol, en su edición Nº 003, tercera del año 2021, resalta a Josué Alejandro Lorca Vega designado como Nuevo Ministro del Poder Popular para el Ecosocialismo.
+El periódico NotiÁrbol, en su edición Nº 004, cuarta del año 2021, resalta la producción de ocumo, plátanos y lechosa por la unidad "Mi Querencia" en Birongo. Ademas de donativos por Misión Árbol del Estado Bolívar de plantas ornamentales para el peaje de Guayana.
 
           </p>
           <p className="text-sm text-gray-700 uppercase tracking-wide font-semibold mt-2">
-            Caracas &bull; 26 de Abril de 2021
+            Caracas &bull; 03 de Mayo de 2021
           </p>
                 
 <div className="inline-block mr-2 mt-2">
  
-    <Link to="https://drive.google.com/file/d/1JI8BdSLoZskHUtcmigE2ygiKMduHZyWV/view?usp=sharing" >
+    <Link to="https://drive.google.com/file/d/1yqoKibg5yJ5k_2xPUJwP4ROcHwObaahe/view?usp=sharing" >
       <button type="button" className="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-blue-600 hover:bg-blue-500 hover:shadow-lg flex items-center">
                        <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                         Descargar
--- src/data/periodico.js
+++ src/data/periodico.js
@@ -2,21 +2,27 @@ import noticia01 from '../../static/assets/notiarbol/notiarbol001.pdf'
 
 export const periodicoData = [
 {
-    title: 'Edición 003 del periódico NotiÁrbol resalta a Josué Alejandro Lorca Vega designado como Nuevo Ministro del Poder Popular para el Ecosocialismo',
-    title2: 'El periódico NotiÁrbol, en su edición Nº 003, tercera del año 2021, resalta a Josué Alejandro Lorca Vega designado como Nuevo Ministro del Poder Popular para el Ecosocialismo.',
-    fecha: '26/04/2021 01:00 p.m',
+    title: 'Edición 004 del periódico NOTIÁRBOL resalta la producción de ocumo, plátanos y lechosa por la unidad "Mi Querencia" en Birongo.',
+    title2: 'El periódico NOTIÁRBOL, en su edición Nº 004, cuarta del año 2021, resalta la producción de ocumo, plátanos y lechosa por la unidad "Mi Querencia" en Birongo. Ademas de donativos por Misión Árbol del Estado Bolívar de plantas ornamentales para el peaje de Guayana.',
+    fecha: '03/05/2021',
+    ruta: `https://drive.google.com/file/d/1yqoKibg5yJ5k_2xPUJwP4ROcHwObaahe/view?usp=sharing`, 
+  },
+{
+    title: 'Edición 003 del periódico NOTIÁRBOL resalta a Josué Alejandro Lorca Vega designado como Nuevo Ministro del Poder Popular para el Ecosocialismo',
+    title2: 'El periódico NOTIÁRBOL, en su edición Nº 003, tercera del año 2021, resalta a Josué Alejandro Lorca Vega designado como Nuevo Ministro del Poder Popular para el Ecosocialismo.',
+    fecha: '26/04/2021',
     ruta: `https://drive.google.com/file/d/1JI8BdSLoZskHUtcmigE2ygiKMduHZyWV/view?usp=sharing`, 
   },
 {
-    title: 'Edición 002 del periódico NotiÁrbol resalta la partida fisica de nuestro Ministro Oswaldo Barbera hacia los bosques de la eternidad',
-    title2: 'El periódico NotiÁrbol, en su edición Nº 002, segunda del año 2021, resalta la partida fisica de nuestro Ministro Oswaldo Barbera hacia los bosques de la eternidad.',
-    fecha: '29/03/2021 01:00 p.m',
+    title: 'Edición 002 del periódico NOTIÁRBOL resalta la partida fisica de nuestro Ministro Oswaldo Barbera hacia los bosques de la eternidad',
+    title2: 'El periódico NOTIÁRBOL, en su edición Nº 002, segunda del año 2021, resalta la partida fisica de nuestro Ministro Oswaldo Barbera hacia los bosques de la eternidad.',
+    fecha: '29/03/2021',
     ruta: `https://cdn-124.anonfiles.com/rd3b59sbu4/e6d93878-1619121652/notiarbol-abril.pdf`, 
   },
   {
-    title: 'Edición 001 del periódico NotiÁrbol resalta el plan de desinfección que se llevo a cabo en el Parque los Caobos en Caracas',
-    title2: 'El periódico NotiÁrbol, en su edición Nº 001, primera del año 2021, resalta el plan de desinfección que ha venido cumpliendo con la meta de proteger nuestro pueblo del covid-19.',
-    fecha: '29/03/2021 01:00 p.m',
+    title: 'Edición 001 del periódico NOTIÁRBOL resalta el plan de desinfección que se llevo a cabo en el Parque los Caobos en Caracas',
+    title2: 'El periódico NOTIÁRBOL, en su edición Nº 001, primera del año 2021, resalta el plan de desinfección que ha venido cumpliendo con la meta de proteger nuestro pueblo del covid-19.',
+    fecha: '29/03/2021',
     ruta: noticia01, 
   },
 ]
