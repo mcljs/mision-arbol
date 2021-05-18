328a804937771e0c021293cef575dc5d80e16d07
Michael Chacon <mcljs15@gmail.com>
2021-05-17 23:45:12 -0400

    fix

--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -52,7 +52,7 @@ export default function ScrollToTop() {
           className="fixed p-2 rounded-lg bg-yellow-1100 bottom-16 right-3 lg:bottom-20 lg:right-5 cursor-pointer dark:bg-[#98ca3f] dark:text-[#03091e]"
         >
 {colorTheme === 'light' ? (
-         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow-300 dark:text-[#03091e]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 dark:text-[#03091e]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
 </svg>  ) : (
 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
