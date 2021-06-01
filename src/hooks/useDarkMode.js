c052d69921480c87ec6a85605d21c44c94a6926a
Michael Chacon <mcljs15@gmail.com>
2021-05-31 20:23:28 -0400

    localstorage darkmode

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,22 @@
 import * as React from "react";
 
+const getInitialTheme = _ => {
+  if (typeof window !== 'undefined' && window.localStorage) {
+    const storedPrefs = window.localStorage.getItem('color-theme')
+    if (typeof storedPrefs === 'string') {
+      return storedPrefs
+    }
+
+    const userMedia = window.matchMedia('(prefers-color-scheme: dark)')
+    if (userMedia.matches) {
+      return 'dark'
+    }
+  }
+
+  // If you want to use light theme as the default, return "light" instead
+  return 'dark'
+}
+
 export default function useDarkMode() {
   const [theme, setTheme] = React.useState();
   const colorTheme = theme === "light" ? "dark" : "light";
@@ -9,6 +26,7 @@ export default function useDarkMode() {
 
     root.classList.remove(colorTheme)
     root.classList.add(theme);
+localStorage.setItem('color-theme', theme)
   }, [theme,colorTheme]);
 
   return [colorTheme, setTheme];
