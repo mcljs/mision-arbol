99910d7de02792e7693a041bde677bf4eb69b6db
Michael Chacon <mcljs15@gmail.com>
2021-09-19 02:05:15 -0400

    add cms-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -168,6 +168,7 @@ flags: {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-netlify-cms`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
